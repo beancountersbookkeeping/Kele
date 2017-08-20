8a60e50d8571965782fcb6afdda0f0ad7b3c9b6c
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-08-20 15:10:54 -0400

    step3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,18 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
 
-  def initialize(username, password)
-    response = self.class.post(api_url("sessions"), body: {"username": username, "password": password})
+  def initialize(email, password)
+    response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
     @auth_token = response["auth_token"]
   end
+
+  def get_me
+   response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+   @user_data = JSON.parse(response.body)
+ end
 
   private
 
